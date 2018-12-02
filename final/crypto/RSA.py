c7783215d4e527bfa262f5096f40a84f42f6fd42
Daniel Petti <djpetti@gmail.com>
2018-12-01 19:56:52 -0500

    Make RSA work with client and server.

--- final/crypto/RSA.py
+++ final/crypto/RSA.py
@@ -1,4 +1,4 @@
-from cryptosystem import Pkc
+from .cryptosystem import Pkc
 from bitarray import *
 import secrets
 import math
@@ -12,6 +12,33 @@ class RSA(Pkc):
         self.e = 0
         self.d = 0
         self.n = 0
+
+    def __to_int(self, message):
+      """ Converts a string message to an int.
+      Args:
+        message: The string message to convert.
+      Returns:
+        The message in int form. """
+      # First, convert the message to a byte array.
+      byte_message = message.encode("latin-1")
+      if len(byte_message) > self.keysize // 8:
+        raise ValueError("Message of length %d must be less than key size." % \
+                         (len(byte_message)))
+
+      # Now, convert to an int.
+      return int.from_bytes(byte_message, byteorder="little", signed=False)
+
+    def __from_int(self, message):
+      """ Converts an int message to a string.
+      Args:
+        message: The int message to convert.
+      Returns:
+        The message in string form. """
+      # First, convert the message to a byte array.
+      length = (message.bit_length() + 7) // 8
+      byte_message = message.to_bytes(length, byteorder="little", signed=False)
+      # Now, convert to a string.
+      return byte_message.decode("latin-1")
 
     def is_prime_MR(self, n, k):
         """ Tests a number to be prime using the Miller Rabin Primality
@@ -80,63 +107,65 @@ class RSA(Pkc):
     def encrypt_public(self, message):
         """ Encrypts a message using the public key.
         Args:
-            message (int): The message to encrypt.
+            message (string): The message to encrypt.
         Returns:
-            (int) The encrypted message. """
-        enc = pow(message, self.e, self.n)
-        return enc
+            (string) The encrypted message. """
+        enc = pow(self.__to_int(message), self.e, self.n)
+        return self.__from_int(enc)
 
     def encrypt_private(self, message):
         """ Encrypts a message using the private key.
         Args:
-            message (int): The message to encrypt.
+            message (string): The message to encrypt.
         Returns:
-            (int) The encrypted message. """
-        return pow(message, self.d, self.n)
+            (string) The encrypted message. """
+        enc = pow(self.__to_int(message), self.d, self.n)
+        return self.__from_int(enc)
 
     def decrypt_public(self, message):
         """ Decrypts a message using the public key.
         Args:
-            message (int): The message to decrypt.
+            message (string): The message to decrypt.
         Returns:
-            (int) The decrypted message. """
-        return pow(message, self.e, self.n)
-
+            (string) The decrypted message. """
+        dec = pow(self.__to_int(message), self.e, self.n)
+        return self.__from_int(dec)
 
     def decrypt_private(self, message):
         """ Decrypts a message using the private key.
         Args:
-            message (int): The message to decrypt.
+            message (string): The message to decrypt.
         Returns:
-            (int) The decrypted message. """
-        intval = pow(message, self.d, self.n)
-        return intval
+            (string) The decrypted message. """
+        dec = pow(self.__to_int(message), self.d, self.n)
+        return self.__from_int(dec)
 
     def gen_key_pair(self):
         """ Generates a random public-private key pair suitable for this
         cryptosystem.
         Returns:
-            (int,int) The public key and the private key. """
+            (list[int,int],int) The public key[e, modulus] and the private
+            key. """
         found_pair = False
         while found_pair == False:
-            self.p = c.rand_prime(int(self.keysize/2+1))
-            self.q = c.rand_prime(int(self.keysize/2+1))
+            self.p = self.rand_prime(int(self.keysize/2+1))
+            self.q = self.rand_prime(int(self.keysize/2+1))
             self.e = secrets.randbits(self.keysize)
             self.d = self.invert(self.e, (self.p - 1)*(self.q - 1))
             if self.d != None: found_pair = True
         self.n = self.p*self.q
-        return self.e, self.d
+        return (self.e, self.n), self.d
 
     def get_key_pair(self):
         """ Get the public and private key materials
         Returns:
-            (list[int,int],int) Public key[e, modulous], private key """
-        return list(self.e, self.n), self.d
+            (list[int,int],int) Public key[e, modulus], private key """
+        return [self.e, self.n], self.d
 
     def set_key_pair(self, npub, npriv):
         """ Sets the new key for this object
         Args:
-            npub (int): new public key
+            npub (list[int, int]): new public key[e, modulus]
             npriv (int): new private key
         Returns
             None. """
@@ -145,23 +174,24 @@ class RSA(Pkc):
         self.n = npub[1]
         return
 
-    def copy_with_public_key(self):
+    def copy_with_public_key(self, pub_key):
         """ Creates a copy of this same cryptosystem, but with a new public key.
-        The private key is set to null.
+        The private key is set to None.
         Args:
           pub_key (list[int,int]): The public key to use.
         Returns:
           The new cryptosystem. """
-        nc = RSA(self.keysize)
-        rc.set_key_pair(list(self.e,self.n),null)
+        rc = RSA(self.keysize)
+        rc.set_key_pair(pub_key, None)
         return rc
 
-
-    def get_name():
+    @classmethod
+    def get_name(cls):
         """ Returns the unique name for this cryptosystem """
         return "RSA"
 
-    def get_priority():
+    @classmethod
+    def get_priority(cls):
         """ Returns the priority for this cryptosystem """
         return 1
 
